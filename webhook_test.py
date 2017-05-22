541de3ad6cca5a7bce76afcccc2d126344a52c3d
aescoda <alejandro.escoda.umh@gmail.com>
2017-05-22 11:16:06 +0200

    Update webhook_test.py

--- webhook_test.py
+++ webhook_test.py
@@ -16,8 +16,7 @@ abuffer = {"sessionId":"","confident":"", "message":"","action":"",
 # Message Received from Jasper
 @app.route('/webhook', methods=['POST','GET'])
 def webhook():
-    # Every message from Jasper is received here. I will be analyzed and sent to
-    # api.ai response will then sent back to Spark
+    
     #req = request.get_json(silent=True, force=True)
     #res = spark_webhook(req)
     print("FUNCIONA!")
