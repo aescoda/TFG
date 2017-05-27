fa723aa6632ba1a47104007f9f0ebc3f78064e87
aescoda <alejandro.escoda.umh@gmail.com>
2017-05-27 19:55:46 +0200

    Add files via upload

---
+++ Emailing.py
@@ -0,0 +1,29 @@
+    #!/usr/bin/env python
+    # -*- coding: utf-8 -*-
+
+
+from email.mime.text import MIMEText
+from smtplib import SMTP
+
+def mail_checking():
+    from_address = "migsepulcre@gmail.com"
+    to_address = "alejandro.escoda.umh@gmail.com"
+    message = "Hello, world!"
+    mime_message = MIMEText(message, "plain")
+    mime_message["From"] = from_address
+    mime_message["To"] = to_address
+    mime_message["Subject"] = "Correo de prueba"
+    smtp = SMTP('smtp.gmail.com', 587)
+    smtp.ehlo()
+    smtp.starttls()
+    smtp.ehlo()
+    print "Conexion exitosa con Gmail"
+    print "Concectado a Gmail"
+    #
+    smtp.login(from_address, "Ym2wlar!")
+    smtp.sendmail(from_address, to_address, mime_message.as_string())
+    smtp.quit()
+    print "fin"
+    return None
+
+mail_checking();
