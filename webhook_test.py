3582505dd07dbcd2615147ddf741a368e47a4a55
aescoda <alejandro.escoda.umh@gmail.com>
2017-05-27 15:40:33 +0200

    Update webhook_test.py

--- webhook_test.py
+++ webhook_test.py
@@ -1,26 +1,13 @@
-import json
 from email.mime.text import MIMEText
 from smtplib import SMTP
 from flask import Flask
-from flask import request
-from flask import make_response
 
 # Flask app should start in global layout
 app = Flask(__name__)
 
-# Buffer for capturing messages from Jasper
-sbuffer = {"sessionId":"","roomId":"","message":"",
-           "personId":"","personEmail":"","displayName":""}
-# Buffer for capturing messages from email
-abuffer = {"sessionId":"","confident":"", "message":"","action":"",
-
 # Message Received from Jasper
 @app.route('/webhook', methods=['POST','GET'])
 def webhook():
-    
-    #req = request.get_json(silent=True, force=True)
-    #res = spark_webhook(req)
-
         from_address = "migsepulcre@gmail.com"
         to_address = "alejandro.escoda.umh@gmail.com"
         message = "Hello, world!"
@@ -38,5 +25,8 @@ def webhook():
         smtp.login(from_address, "Ym2wlar!")
         smtp.sendmail(from_address, to_address, mime_message.as_string())
         smtp.quit()
+        return none
 
-         return none
+if __name__ == "__main__":
+           app.run
+           
