27b4d0b77942afaf6ef87d012311c651f5e373cb
aescoda <alejandro.escoda.umh@gmail.com>
2017-05-27 20:41:05 +0200

    Update webhook_test.py

--- webhook_test.py
+++ webhook_test.py
@@ -4,16 +4,18 @@
 
 from flask import Flask
 import emailing
+import os
 
 # Flask app should start in global layout
 app = Flask(__name__)
 
 @app.route('\webhook', methods=['POST','GET'])
 def webhook:
-   mail_cheking();
-   return none
+    mail_cheking();
+    return none
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.getenv('PORT', 5000))
+    app.run(debug=True, port=port, host='0.0.0.0', threaded=True)
     
     
