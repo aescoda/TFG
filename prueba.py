beb03616642b8215c10e6c76169fe02542a26d1f
aescoda <alejandro.escoda.umh@gmail.com>
2017-05-27 20:35:28 +0200

    Update prueba.py

--- prueba.py
+++ prueba.py
@@ -6,4 +6,5 @@ def hello():
     return "Hola mundo"
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.getenv('PORT', 5000))
+    app.run(debug=True, port=port, host='0.0.0.0', threaded=True)
