1be95d92aedac96a4b25de2d816aef03298704f9
aescoda <alejandro.escoda.umh@gmail.com>
2017-05-27 19:57:54 +0200

    Update webhook_test.py

--- webhook_test.py
+++ webhook_test.py
@@ -3,34 +3,17 @@
 
 
 from flask import Flask
-from email.mime.text import MIMEText
-from smtplib import SMTP
+import emailing
 
 # Flask app should start in global layout
 app = Flask(__name__)
 
 @app.route('/webhook', methods=['POST','GET'])
 def webhook():
-    from_address = "migsepulcre@gmail.com"
-    to_address = "alejandro.escoda.umh@gmail.com"
-    message = "Hello, world!"
-    mime_message = MIMEText(message, "plain")
-    mime_message["From"] = from_address
-    mime_message["To"] = to_address
-    mime_message["Subject"] = "Correo de prueba"
-    smtp = SMTP('smtp.gmail.com', 587)
-    smtp.ehlo()
-    smtp.starttls()
-    smtp.ehlo()
-    print "Conexion exitosa con Gmail"
-    print "Concectado a Gmail"
-    #
-    smtp.login(from_address, "Ym2wlar!")
-    smtp.sendmail(from_address, to_address, mime_message.as_string())
-    smtp.quit()
-    print "fin"
-    return None
+    mail_cheking();
+    return none
+
 if __name__ == '__main__':
-    app.run(debug=True, port=5000, host='0.0.0.0', threaded=True)
+    app.run()
     
     
