3ccbac6c5f12f05d40dbef90d723380e0b5b5963
aescoda <alejandro.escoda.umh@gmail.com>
2017-05-28 12:54:04 +0200

    Update prueba.py

--- prueba.py
+++ prueba.py
@@ -2,8 +2,8 @@ from flask import Flask
 import os
 app = Flask(__name__)
 
-@app.route("/", methods=["POST","GET"])
-def hello():
+@app.route("/webhook", methods=["POST","GET"])
+def webhook():
     return "Hola mundo"
 
 if __name__ == "__main__":
