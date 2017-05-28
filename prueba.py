45e6bfcd6c2c6c108a58dd2aa1ab39dba5b77f6a
aescoda <alejandro.escoda.umh@gmail.com>
2017-05-28 17:12:41 +0200

    Update prueba.py

--- prueba.py
+++ prueba.py
@@ -1,27 +1,16 @@
 from flask import Flask
+from flask import request
+from flask import make_response
 import os
-from email.mime.text import MIMEText
-from smtplib import SMTP
 
 
 app = Flask(__name__)
 
-@app.route("/webhook", methods=["POST","GET"])
+@app.route('/webhook', methods=['POST','GET'])
 def webhook():
     print "Email1"
-    from_address = "migsepulcre@gmail.com"
-    print"Email1"
-    print "email2"
-    to_address = "alejandro.escoda.umh@gmail.com"
-    print "email2"
-    message = "Hello, world!"
-    mime_message = MIMEText(message, "plain")
-    mime_message["From"] = from_address
-    mime_message["To"] = to_address
-    mime_message["Subject"] = "Correo de prueba"
-    print "Ha salido"
     return None
 
 if __name__ == "__main__":
     port = int(os.getenv('PORT', 5000))
-    app.run(debug=False, port=port, host='0.0.0.0', threaded=True)
+    app.run(debug=True, port=port, host='0.0.0.0', threaded=True)
