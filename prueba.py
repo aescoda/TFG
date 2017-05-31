f3f6d748960ac99471260ce3001da79445d421ab
aescoda <alejandro.escoda.umh@gmail.com>
2017-05-31 11:43:43 +0200

    Update prueba.py

--- prueba.py
+++ prueba.py
@@ -11,7 +11,7 @@ def webhook():
     # Every message from Spark is received here. I will be analyzed and sent to
     # api.ai response will then sent back to Spark
     print "email"
-    return "You"
+    return None
 
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
