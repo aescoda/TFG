a99cfaeade76bd7d8ba90a375a7a0534abe528ab
aescoda <alejandro.escoda.umh@gmail.com>
2017-05-31 16:42:54 +0200

    Update prueba.py

--- prueba.py
+++ prueba.py
@@ -14,7 +14,7 @@ def webhook():
     print "email"
     req = request.data
     print req
-    tree = ElementTree.ElementTree(ElementTree.fromstring(req))
+    tree = ET.ElementTree(ET.fromstring(req))
     print "ha hecho el XML"
     nombre = data.findall('comida/nombre')
     print "ha encontrado"
