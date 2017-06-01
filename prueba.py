59f97f569f05d748d24e9be652139dc73a62743d
aescoda <alejandro.escoda.umh@gmail.com>
2017-06-01 15:12:49 +0200

    Update prueba.py

--- prueba.py
+++ prueba.py
@@ -12,7 +12,7 @@ def webhook():
     # Every message from Spark is received here. I will be analyzed and sent to
     # api.ai response will then sent back to Spark
     print "email"
-    req = request.get_data(cache=True, as_text=False, parse_form_data=False)
+    req = request.form
     cad = len(req)
     print cad
     print req
