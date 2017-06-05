1f354936bd13d319abe04e031a52adcf0f3e53d1
aescoda <alejandro.escoda.umh@gmail.com>
2017-06-05 16:27:22 +0200

    Update prueba.py

--- prueba.py
+++ prueba.py
@@ -1,18 +1,28 @@
 from flask import Flask
 from flask import request
-from flask import make_response
+from flask import make_response #Este no se si lo necesito
 import os
 import xml.etree.ElementTree as ET
+from threading import Thread
+
 
 
 app = Flask(__name__)
+
+def get_email(xml):
+    data = ET.fromstring(xml)
+    iccid = req[0]
+    get_admin()
+    
+
 
 @app.route('/webhook', methods=['POST','GET'])
 def webhook():
     req = request.form
     xml = req['data']
-    
-    return ""
+    t = Thread(target=emailing, args=(xml,))
+    t.start()
+    return '',200
    
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
