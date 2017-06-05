b8a6e2272f8a2c500183e09e3c312df543c562a5
aescoda <alejandro.escoda.umh@gmail.com>
2017-06-05 15:37:15 +0200

    Update prueba.py

--- prueba.py
+++ prueba.py
@@ -3,7 +3,6 @@ from flask import request
 from flask import make_response
 import os
 import xml.etree.ElementTree as ET
-import Emailing
 
 
 app = Flask(__name__)
@@ -12,17 +11,7 @@ app = Flask(__name__)
 def webhook():
     req = request.form
     xml = req['data']
-    #Mando un codigo 200 para que esto no pete?
-    get_email()
-    #Ahora es cuando hago un get a Jasper para conseguir el email de mi cliente
-    email_alert(email)
-    doc0 = ET.fromstring(xml)
-    print doc0[0].text
-    print doc0[1].text
-    print doc0[2].text
-    print doc0[3].text
     
-    print "VAMOS A SACAR LOS PUTOS DATOS"
     return ""
    
 if __name__ == '__main__':
