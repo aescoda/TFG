56c29c5f62a533a412a35cf8144b7baa9ec31cbe
aescoda <alejandro.escoda.umh@gmail.com>
2017-06-10 11:27:10 +0200

    Create prueba.py

--- prueba.py
+++ prueba.py
@@ -12,38 +12,6 @@ app = Flask(__name__)
 
 def send_email(xml):
     
-    service = 'terminal'
-    wsdlurl= os.environ.get('WSDL_FILE', None)
-    username= os.environ.get('USER_NAME', None)
-    password= os.environ.get('USER_PASS', None)
-    license_key= os.environ.get('LICENSE_KEY', None)
-
-
-    print("***** create the SOAP client")
-    clientService = Client(wsdlurl)
-    #print clientService
-
-    print("***** compose the SOAP security header")
-    # add WSSE
-    security = Security()
-    token = UsernameToken(username, password)
-    security.tokens.append(token)
-    clientService.set_options(wsse=security)
-
-    print("***** compose the SOAP body")
-    #define imsi as a complext type
-    iccid =  dict(iccid = '89302720396916796910')
-    getTerminalsDetailsRequest = dict(messageId="1001", version="1.0", licenseKey=license_key, iccid=iccid)
-
-    print("***** SOAP request is:")
-    #print getTerminalsDetailsRequest
-
-    getTerminalsDetailsResponse = clientService.service.GetTerminalDetails(**getTerminalsDetailsRequest)
-
-    print("***** SOAP response is:")
-    print(getTerminalsByImsiResponse)
-    iccid = (getTerminalsDetailsResponse.terminals.terminal[0].accountId)
-    print(iccid)
     return None
     
 
