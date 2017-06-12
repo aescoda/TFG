5c2016abc67c1824efd4f8140a6b756dd224d86f
aescoda <alejandro.escoda.umh@gmail.com>
2017-06-12 18:16:23 +0200

    Create email_lib.py

--- email_lib.py
+++ email_lib.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 from email.mime.text import MIMEText
 from smtplib import SMTP
+import os
 
-sender_email = os.environ.get('MAIL_USER', None)
-sender_pass = os.environ.get('MAIL_PASS', None)
-server_email = os.environ.get('MAIL_SERVER', None)
-port_email = os.environ.get('MAIL_PORT', None)
-SSL_email = os.environ.get('MAIL_SSL', None)   
+#sender_email = os.environ.get('MAIL_USER', None)
+#sender_pass = os.environ.get('MAIL_PASS', None)
+#server_email = os.environ.get('MAIL_SERVER', None)
+#port_email = os.environ.get('MAIL_PORT', None)
+#SSL_email = os.environ.get('MAIL_SSL', None)   
 
 def prueba():
     print "INCLUIDO"
