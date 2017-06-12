9af8eab77057718f4e8c67aa0daf731cc0360139
aescoda <alejandro.escoda.umh@gmail.com>
2017-06-12 18:02:33 +0200

    Create email_lib.py

--- email_lib.py
+++ email_lib.py
@@ -8,7 +8,8 @@ server_email = os.environ.get('MAIL_SERVER', None)
 port_email = os.environ.get('MAIL_PORT', None)
 SSL_email = os.environ.get('MAIL_SSL', None)   
 
-
+def prueba():
+    print "INCLUIDO"
 
 def email_alert(recipient, iccid, customer):
     message = "<br>Hola %s,<br><br> Se ha detectado una alerta de seguridad de cambio de IMEI en la SIM con iccid = %s<br><br> Si este cambio de IMEI ha sido voluntario, por favor, ignore este mensaje. De lo contrario por favor hago acceda al siguiente link para localizar su SIM y desactivarla: <br><br> <center><a href='https://jasper-alert.herokuapp.com/response'> Localizar y desactivar SIM </a><br><br></center>    Muchas gracias,<br><br>    Equipo de Cisco Jasper<br>" % (customer, iccid)
