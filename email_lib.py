56c1698bd732fe73c92c2f1eeb0b13e55c06d0e9
aescoda <alejandro.escoda.umh@gmail.com>
2017-06-13 12:30:39 +0200

    Create email_lib.py

--- email_lib.py
+++ email_lib.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 ##This file contains functions to interact with the customer with email in the main.py file##
-
+import geocode
 from email.mime.text import MIMEText
 from smtplib import SMTP
 import os
