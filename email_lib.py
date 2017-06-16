50d8bc25e50326e5da534400b7d0d17a5944f001
aescoda <alejandro.escoda.umh@gmail.com>
2017-06-16 12:56:21 +0200

    Update email_lib.py

--- email_lib.py
+++ email_lib.py
@@ -18,16 +18,16 @@ SSL_email = os.environ.get('MAIL_SSL', None)
 def email_alert(recipient, iccid, customer,event):
     #We create the email for customer's name and iccid
     if event == "SIM_STATE_CHANGE":
-        message = 
+        message = "<br>Estimado usuario,<br><br> Se ha detectado una cambio de estado de su tarjeta SIM con numero de identificacion = %s<br><br> Si este cambio ha sido voluntario, por favor, ignore este mensaje. De lo contrario por favor hago acceda al siguiente link para reactivar su tarjeta SIM: <br><br> <center><a href='https://jasper-alert.herokuapp.com/response'> Reactivar SIM </a><br><br></center>    Muchas gracias,<br><br>    Equipo de Cisco Jasper<br> % (customer, iccid, address, location[0],location[1])
         subject = "SIM state change alert"
     elif event == "IMEI_CHANGE":
-        message = "<br>Hola %s,<br><br> Se ha detectado una alerta de seguridad de cambio de IMEI en la SIM con iccid = %s<br><br> Si este cambio de IMEI ha sido voluntario, por favor, ignore este mensaje. De lo contrario por favor hago acceda al siguiente link para localizar su SIM y desactivarla: <br><br> <center><a href='https://jasper-alert.herokuapp.com/response'> Localizar y desactivar SIM </a><br><br></center>    Muchas gracias,<br><br>    Equipo de Cisco Jasper<br>" % (customer, iccid)
+        message = "<br>Estimado usuario,<br><br> Se ha detectado una alerta de seguridad de cambio de IMEI de su tarjeta SIM con numero de identificacion = %s<br><br> Si este cambio de IMEI ha sido voluntario, por favor, ignore este mensaje. De lo contrario por favor hago acceda al siguiente link para localizar su tarjeta SIM y desactivarla: <br><br> <center><a href='https://jasper-alert.herokuapp.com/response'> Localizar y desactivar SIM </a><br><br></center>    Muchas gracias,<br><br>    Equipo de Cisco Jasper<br>" % (customer, iccid)
         subject = "IMEI change alert"
     elif event == "DATA_LIMIT"
-        message = 
+        message =  "Estimado usuario, Se ha alcanzado el limite de datos contratados en su contrato. Si desea contratar un paquete adicional, por favor, acceda al siguiente link: 
         subject = "Data limit achived"
     elif event == "PAST24H_SESSION_USAGE_LESSTHAN"
-        message =
+        message = "Estimado usuario, Se han detectado un numero elevado numero de conexiones en esta tarjeta SIM, con numero de identificación = %s. Por favor, revise la alerta identificada con el equipo de seguridad de Cisco Jasper, Atentament
         subject = "Session usage alert"
     else
         message = ""
@@ -41,13 +41,10 @@ def email_alert(recipient, iccid, customer,event):
     smtp.ehlo()
     smtp.starttls()
     smtp.ehlo()
-    print "Conexion exitosa con Gmail"
-    print "Concectado a Gmail"
     #We introduce the credentials of the sender in the SMTP server and send the email.
     smtp.login(sender_email, sender_pass)
     smtp.sendmail(sender_email, recipient, body)
     smtp.quit()
-    print "fin"
     return "Mensaje enviado"
 
 #This function is used to send the location and iccid of the SIM card that has been changed.
@@ -57,13 +54,13 @@ def email_action (recipient, customer, data):
         message = 
         subject = "SIM state change alert"
     elif event == "IMEI_CHANGE":
-        message = "<br>Hola %s,<br><br> Se ha detectado una alerta de seguridad de cambio de IMEI en la SIM con iccid = %s<br><br> Si este cambio de IMEI ha sido voluntario, por favor, ignore este mensaje. De lo contrario por favor hago acceda al siguiente link para localizar su SIM y desactivarla: <br><br> <center><a href='https://jasper-alert.herokuapp.com/response'> Localizar y desactivar SIM </a><br><br></center>    Muchas gracias,<br><br>    Equipo de Cisco Jasper<br>" % (customer, iccid)
+        message = "<br>Hola %s,<br><br> Tras su confirmacion de que ha habido un acceso no autorizado a la SIM con iccid = %s hemos procedido a su localizacion y desactivacion. <br><br> Su ultima posicion antes de la desactivacion era %s, en las coordenadas exactas de %s, %s <br><br> Rogamos se ponga en contacto con el equipo de Jasper para investigar en profundidad el problema<br><br>   Muchas gracias,<br><br>    Equipo de Cisco Jasper<br>" % (customer, data[2], data[3], data[0],data[1])
         subject = "IMEI change alert"
     elif event == "DATA_LIMIT"
         message = 
         subject = "Data limit achived"
     elif event == "PAST24H_SESSION_USAGE_LESSTHAN"
-        message =
+        message = "Estimado usuario, 
         subject = "Session usage alert"
     else
         message = ""
@@ -78,13 +75,10 @@ def email_action (recipient, customer, data):
     smtp.ehlo()
     smtp.starttls()
     smtp.ehlo()
-    print "Conexion exitosa con Gmail"
-    print "Concectado a Gmail"
     #We introduce the credentials of the sender in the SMTP server and send the email.
     smtp.login(sender_email, sender_pass)
     smtp.sendmail(sender_email, recipient, body)
     smtp.quit()
-    print "fin"
     return "Mensaje enviado"
 
 
