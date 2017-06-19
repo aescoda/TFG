69b58a714ea381c2de4803ef729e893ce9e75269
aescoda <alejandro.escoda.umh@gmail.com>
2017-06-19 10:15:04 +0200

    Update email_lib.py

--- email_lib.py
+++ email_lib.py
@@ -23,10 +23,10 @@ def email_alert(recipient, iccid, event):
     elif event == "IMEI_CHANGE":
         message = "<br>Estimado usuario,<br><br> Se ha detectado una <b>alerta de seguridad de cambio de IMEI de su tarjeta SIM con numero de identificacion = %s</b><br><br> Si este cambio de IMEI ha sido voluntario, por favor, ignore este mensaje. De lo contrario por favor hago acceda al siguiente link para localizar su tarjeta SIM y desactivarla: <br><br> <center><a href='https://jasper-alert.herokuapp.com/response'> Localizar y desactivar SIM </a><br><br></center>Atentamente,<br><br>Equipo de Cisco Jasper<br><img src ='https://assets.sdxcentral.com/cisco-jasper-control-center-product.png'>" % (iccid)
         subject = "Alerta cambio de IMEI"
-    elif event == "DATA_LIMIT"
+    elif event == "DATA_LIMIT":
         message = "<br>Estimado usuario,<br><br> Se ha alcanzado el limite de datos disponibles en su contrato. Si desea contratar un paquete adicional, por favor, acceda al siguiente link: <br><br>   <left><a href='https://nj.jasperwireless.com/provision/ui/billing/rateplans/manage/details.html?detailFbRatePlanId=975801&popup=true'> Ampliar suscripcion </a><br><br></left>  Para obtener mas informacion del uso realizado de sus datos haga click aqui:<br><br>  <left><a href='https://jasper-alert.herokuapp.com/response'> Uso de datos </a><br><br></left> Atentamente,<br><br> Equipo de Cisco Jasper<br> <img src ='https://assets.sdxcentral.com/cisco-jasper-control-center-product.png'>"
         subject = "Alerta consumo de datos alcanzado"
-    elif event == "CTD_SESSION_USAGE_EXCEEDED"
+    elif event == "CTD_SESSION_USAGE_EXCEEDED":
         message = "<br>Estimado usuario,<br><br> Se han detectado un numero elevado numero de conexiones en la tarjeta SIM, con numero de identificación = %s.<br><br> Por favor, revise la alerta identificada con su equipo de seguridad de Cisco Jasper, Atentamente,<br><br>, Equipo de Cisco Jasper<br><img src ='https://assets.sdxcentral.com/cisco-jasper-control-center-product.png'>" % (iccid)
         subject = "Alerta de conexiones"
     #We create thebody with the message depending on the event
@@ -54,7 +54,7 @@ def email_action (recipient, data):
     elif event == "IMEI_CHANGE":
         message = "<br>Estimado usuario,<br><br> Tras su confirmacion de un acceso no autorizado a la SIM con iccid = %s hemos procedido a su localizacion y desactivacion. <br><br> La ultima posicion previa a la desactivacion era %s, en las coordenadas exactas de %s, %s <br><br> Rogamos se ponga en contacto con el equipo de seguridad de Jasper para investigar en detalle el evento ocurrido<br><br>   Atentamente,<br><br>    Equipo de Cisco Jasper<br><img src ='https://assets.sdxcentral.com/cisco-jasper-control-center-product.png'>" % (data[3], data[2], data[0],data[1])
         subject = "Localizacion y desactivacion exitosas"
-    elif event == "DATA_LIMIT"
+    elif event == "DATA_LIMIT":
         message = "<br>Estimado usuario, Encuentre a continuacion los datos de consumo solicitados previamente: <br><br> Volmen de datos usados: %s <br><br> Fecha de incio: %s <br><br> Volmen de datos usados (SMS): %s <br><br> Volmen de datos usados (Voz): %s<br><br> Atentamente,<br><br>    Equipo de Cisco Jasper<br><img src ='https://assets.sdxcentral.com/cisco-jasper-control-center-product.png'>" % (data[0], data[1], data[2], data[3])
         subject = "Detalles de consumo"
     #We create thebody with the message depending on the event
