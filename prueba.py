a8735ff29803ff92fdfca2e427e89edc07e890f2
aescoda <alejandro.escoda.umh@gmail.com>
2017-06-19 13:35:21 +0200

    Update prueba.py

--- prueba.py
+++ prueba.py
@@ -35,7 +35,7 @@ def alert():
     
 @app.route('/response', methods=['POST','GET'])
 def response():
-    ter = Terminals()
+    ter = jasper_lib.Terminals()
     ter.prueba() #Comprobar como comparto la variable.
     return "Acabamos de procesar su peticion, en breve recibira un email con los detalles"
     
