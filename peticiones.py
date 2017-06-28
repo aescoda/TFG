93f6fc47805ee5ccf825f954ee68c2cc9f60bebf
aescoda <alejandro.escoda.umh@gmail.com>
2017-06-28 10:52:42 +0200

    Update peticiones.py

--- peticiones.py
+++ peticiones.py
@@ -2,7 +2,10 @@
 # -*- coding: utf-8 -*-
 import geocoder
 
+g = geocoder.google('Av. de la Vega, 15, 28108 Alcobendas, Madrid')
+print g.latlng
+
 location = [38.2749014, -0.6842547] 
-address = geocoder.google(location, method='reverse')
-print str(address)
+#address = geocoder.google(location, method='reverse')
+#print str(address)
 
